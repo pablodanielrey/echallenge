12c0f7541d62b262b5fa2680613a3b86a6b3f4ed
Pablo Daniel Rey <pablodanielrey@gmail.com>
2022-05-11 01:10:52 -0300

    refactorizo para manejar los 2 tipos de bases para detecciones

--- api/src/indexer/api/indexer/deps.py
+++ api/src/indexer/api/indexer/deps.py
@@ -2,17 +2,30 @@ from fastapi import Depends
 
 from . import settings, models
 
-from indexer.vehicles import models as v_models, db as v_db
+from indexer.vehicles import models as v_models
 
 
 def _get_config():
     return settings.Settings()
 
 
-def get_vehicles_manager(config: settings.Settings = Depends(_get_config)):
-    db = v_db.DB(config.vehicles_db_connection)
-    vm = v_models.VehiclesManager(db)
-    return vm
+def get_vehicles_manager(config: settings.Settings = Depends(_get_config)) -> v_models.VehiclesManager:
+    """
+        quick and dirty. obtengo el manager de vehículos. 
+        TODO: cambiar este código por un patrón correcto. (Factory o del estilo) - #codesmell
+    """
+    con = config.vehicles_db_connection
+    if con.startswith('postgresql'):
+        from indexer.pgvehicles import db, models as pgmodels
+        pgdb = db.DB(con)
+        return pgmodels.VehiclesManager(pgdb)
+
+    elif con.startswith('mongo'):
+        from indexer.mvehicles import db, models as mmodels
+        mdb = db.DB(con)
+        return mmodels.VehiclesManager(mdb)
+
+    raise Exception('no se conoce el tipo de base de datos')
 
 
 def get_alerts_manager(config: settings.Settings = Depends(_get_config)):
